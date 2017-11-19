f88df2ae98276b0259f04d403868b17bfd780230
Sebastian D. Eastham <seastham@mit.edu>
2017-11-19 15:56:23 -0500

    Updated constants and added molar mass of air and water
    Signed-off-by: Sebastian D. Eastham <seastham@mit.edu>

--- gcpy/constants.py
+++ gcpy/constants.py
@@ -8,10 +8,16 @@ G = 9.80665
 R_EARTH = 6371.0072
 
 #: Avogadro's number, molec/mol
-AVOGADRO = 6.022e23
+AVOGADRO = 6.022140857e+23
 
 #: Universal gas contant, J/mol/K
 R_GAS_UNIV = 8.314
 
 #: Boltzmann's constant, J/K
 K_BOLTZMANN = 1.381e-23
+
+# Typical molar mass of air, kg/mol
+MW_AIR = 28.97e-3
+
+# Molar mass of water, kg/mol
+MW_H2O = 18.016e-3
