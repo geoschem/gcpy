aa25cd9ff572bbcd66691bf61c704b28a31b58a9
Sebastian D. Eastham <seastham@mit.edu>
2017-11-19 13:17:44 -0500

    Implemented calculations of rectilinear grid lat/lon edge vectors
    Signed-off-by: Sebastian D. Eastham <seastham@mit.edu>

--- gcpy/grid/horiz.py
+++ gcpy/grid/horiz.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from numpy import asarray
-#: definitions
+from .. constants import R_EARTH
 
 def calc_rectilinear_lon_edge(lon_stride, center_at_180):
     """ Compute longitude edge vector for a rectilinear grid.
@@ -24,14 +24,12 @@ def calc_rectilinear_lon_edge(lon_stride, center_at_180):
 
     Notes
     -----
-
     All values are forced to be between [-180,180]. For a grid with N cells in
     each band, N+1 edges will be returned, with the first and last value being
     duplicates.
 
     Examples
     --------
-
     >>> from gcpy.grid.horiz import calc_rectilinear_lon_edge
     >>> calc_rectilinear_lon_edge(5.0,true)
     np.array([177.5,-177.5,-172.5,...,177.5])
@@ -42,14 +40,34 @@ def calc_rectilinear_lon_edge(lon_stride, center_at_180):
 
     """
 
-    pass
+    n_lon = np.round(360.0/lon_stride)
+    lon_edge = np.linspace(-180.0,180.0,num=n_lon+1)
+    if center_at_180:
+        lon_edge = lon_edge - (lon_stride/2.0)
+
+    lon_edge[lon_edge<-180.0] = lon_edge[lon_edge<-180] + 360.0
+    lon_edge[lon_edge>180.0] = lon_edge[lon_edge>180.0] - 360.0
+
+    return lon_edge
 
 def calc_rectilinear_lat_edge(lat_stride, half_polar_grid):
     """ Compute latitude edge vector for a rectilinear grid.
 
     """
 
-    pass
+    if half_polar_grid:
+        start_pt = 90.0 + (lat_stride/2.0)
+    else:
+        start_pt = 90.0
+
+    lat_edge = np.linspace(-1.0*start_pt,start_pt,
+        num=1+np.round(2.0*start_pt/lat_stride))
+
+    # Force back onto +/- 90
+    lat_edge[lat_edge>90.0] = 90.0
+    lat_edge[lat_edge<-90.0] = -90.0
+
+    return lat_edge
 
 def calc_rectilinear_grid_area(lon_edge,lat_edge):
     """ Compute grid cell areas (in m2) for a rectilinear grid.
@@ -57,3 +75,43 @@ def calc_rectilinear_grid_area(lon_edge,lat_edge):
     """
     
     pass
+def calc_delta_lon(lon_edge):
+    """ Compute grid cell longitude widths from an edge vector.
+
+    Parameters
+    ----------
+    lon_edge: float
+        Vector of longitude edges, in degrees East.
+
+    Returns
+    -------
+    Width of each cell, degrees East
+
+    Notes
+    -----
+    Accounts for looping over the domain.
+
+    Examples
+    --------
+
+    #TODO
+    """
+
+    n_lon = (lon_edge.size) - 1
+
+    lon_edge = asarray(lon_edge)
+
+    # Set up output array
+    lon_delta = np.zeros((n_lon))
+
+    offset = 0.0
+    next_lon = lon_edge[0]
+    for i_lon in range(0,n_lon):
+        last_lon = next_lon
+        next_lon = lon_edge[i_lon + 1] + offset
+        while next_lon < last_lon:
+            offset = offset + 360.0
+            next_lon = next_lon + 360.0
+        lon_delta[i_lon] = next_lon - last_lon
+
+    return lon_delta
