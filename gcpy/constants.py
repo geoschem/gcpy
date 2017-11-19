9d3dd2fa27d77d398f425c563bf6e3eef5c47ba3
Sebastian D. Eastham <seastham@mit.edu>
2017-11-19 11:31:28 -0500

    Extended constants.py and removed atm_sci/constants.py
    Signed-off-by: Sebastian D. Eastham <seastham@mit.edu>

--- gcpy/constants.py
+++ gcpy/constants.py
@@ -5,3 +5,12 @@ G = 9.80665
 
 #: Radius of the Earth, km
 R_EARTH = 6356.77
+
+#: Avogadro's number, molec/mol
+AVOGADRO = 6.022e23
+
+#: Universal gas contant, J/mol/K
+R_GAS_UNIV = 8.314
+
+#: Boltzmann's constant, J/K
+K_BOLTZMANN = 1.381e-23
