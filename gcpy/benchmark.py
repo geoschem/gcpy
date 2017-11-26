d9c4842f6d252ba9ca7c182ce8cc03a68e296311
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2017-11-26 00:30:11 -0500

    add more comments and TODO list

--- gcpy/benchmark.py
+++ gcpy/benchmark.py
@@ -16,8 +16,8 @@ from .plot import WhGrYlRd, add_latlon_ticks
 mpl.rcParams['font.size'] = 12
 mpl.rcParams['axes.titlesize'] = 20
 
-cmap_abs = WhGrYlRd  # for absolute magnitude
-cmap_diff = 'RdBu_r'  # for difference plot
+cmap_abs = WhGrYlRd  # for plotting absolute magnitude
+cmap_diff = 'RdBu_r'  # for plotting difference
 
 
 def plot_layer(dr, ax, title='', unit='', diff=False):
@@ -41,12 +41,6 @@ def plot_layer(dr, ax, title='', unit='', diff=False):
         Switch to the color scale for difference plot
     '''
 
-    # imshow() is 6x faster than pcolormesh(),
-    # but with some limitations:
-    # - only works with PlateCarree projection
-    # - the left map boundary can't be smaller than -180,
-    #   so the leftmost box (-182.5 for 4x5 grid) is slightly out of the map
-
     assert isinstance(ax, GeoAxes), (
            "Input axis must be cartopy GeoAxes! "
            "Can be created by: \n"
@@ -68,16 +62,20 @@ def plot_layer(dr, ax, title='', unit='', diff=False):
         vmin = 0
         cmap = cmap_abs
 
+    # imshow() is 6x faster than pcolormesh(), but with some limitations:
+    # - only works with PlateCarree projection
+    # - the left map boundary can't be smaller than -180,
+    #   so the leftmost box (-182.5 for 4x5 grid) is slightly out of the map
     im = dr.plot.imshow(ax=ax, vmin=vmin, vmax=vmax, cmap=cmap,
                         transform=ccrs.PlateCarree(),
                         add_colorbar=False)
 
-    # can also pass cbar_kwargs to dr.plot() to add colorbar
+    # can also pass cbar_kwargs to dr.plot() to add colorbar,
     # but it is easier to tweak colorbar afterwards
     cb = fig.colorbar(im, ax=ax, shrink=0.6, orientation='horizontal', pad=0.1)
     cb.set_label(unit)
 
-    # xarray automatically sets a title which might contain dimension data
+    # xarray automatically sets a title which might contain dimension info.
     # surpress it or use user-provided title
     ax.set_title(title)
 
@@ -106,6 +104,7 @@ def plot_zonal(dr, ax, title='', unit='', diff=False):
         Switch to the color scale for difference plot
     '''
 
+    # assume global field from 90S to 90N
     xtick_positions = np.array([-90, -60, -30, 0, 30, 60, 90])
     xticklabels = ['90$\degree$S',
                    '60$\degree$S',
@@ -118,6 +117,8 @@ def plot_zonal(dr, ax, title='', unit='', diff=False):
 
     fig = ax.figure  # get parent figure
 
+    # this code block largely duplicates plot_layer()
+    # TODO: remove duplication
     if diff:
         vmax = np.max(np.abs(dr.values))
         vmin = -vmax
@@ -179,15 +180,14 @@ def make_pdf(ds1, ds2, filename, on_map=True, diff=False,
         plot_func = plot_zonal
         subplot_kw = None
 
-    # plot all the variables in ds1
+    # get a list of all variable names in ds1
     # assume ds2 also has those variables
-
     varname_list = list(ds1.data_vars.keys())
 
     n_var = len(varname_list)
     print('Benchmarking {} variables'.format(n_var))
 
-    n_row = 3  # how many rows per page
+    n_row = 3  # how many rows per page. TODO: should this be input argument?
     n_page = (n_var-1) // n_row + 1  # how many pages
 
     print('generating a {}-page pdf'.format(n_page))
@@ -200,15 +200,19 @@ def make_pdf(ds1, ds2, filename, on_map=True, diff=False,
         fig, axes = plt.subplots(n_row, 2, figsize=[16, 16],
                                  subplot_kw=subplot_kw)
 
-        # a list of variables names with only 3 elements
+        # a list of 3 (n_row) variables names
         sub_varname_list = varname_list[n_row*ipage:n_row*(ipage+1)]
 
         for i, varname in enumerate(sub_varname_list):
             for j, ds in enumerate([ds1, ds2]):
                 plot_func(ds[varname], axes[i][j], unit=unit, diff=diff)
 
+            # TODO: tweak varname, e.g. Trim "TRC_O3" to "O3"
             axes[i][0].set_title(varname+'; '+title1)
             axes[i][1].set_title(varname+'; '+title2)
+
+            # TODO: unit conversion according to data range of each variable,
+            # e.g. mol/mol -> ppmv, ppbv, etc...
 
         pdf.savefig(fig)
         plt.close(fig)  # don't show in notebook!
