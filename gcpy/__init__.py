15394722b4d2831364885cfd1eb27b819a1e9e49
Lizzie Lundgren <elundgren@seas.harvard.edu>
2018-02-15 18:05:42 -0500

    Remove import of __version__ as a workaround for missing version.py
    version.py is not included in gcpy and therefore an ImportError is raised
    during initialization. The filename is included in .gitignore which may
    have caused this bug by preventing version.py from being committed in prior
    versions.
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- gcpy/__init__.py
+++ gcpy/__init__.py
@@ -5,11 +5,11 @@ from __future__ import print_function
 from .core import open_dataset, open_mfdataset
 from . import plot
 
-try:
-    from . version import __version__
-except ImportError:
-    raise ImportError('gcpy was not properly installed; some functionality '
-                      'may be not work. If installing from source code, '
-                      'please re-install in place by running\n'
-                      '$ pip install -e .'
-                      '\nElse, please reinstall using your package manager.')
+#try:
+#    from . version import __version__
+#except ImportError:
+#    raise ImportError('gcpy was not properly installed; some functionality '
+#                      'may be not work. If installing from source code, '
+#                      'please re-install in place by running\n'
+#                      '$ pip install -e .'
+#                      '\nElse, please reinstall using your package manager.')
