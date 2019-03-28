cd10bdd908f3401b55f449121d53819154abe48f
Melissa Sulprizio <mpayer@seas.harvard.edu>
2019-03-28 11:31:32 -0400

    Add bug fix and minor edits to run_1mo_benchmark.py
    Needed to add "benchmark." before "make_gcc_1mo_benchmark_emis_tables".
    
    Also rearranged the settings at the top of the file for easier editing.
    Users should generally only need to change the path to their run directories,
    the strings for ref and dev versions, and the date (YYYYMMDD) of the
    simulation. The name of spcfile, hcofile, etc. shouldn't change often so
    we can use templates and simply fill in the date.
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- benchmark/run_1mo_benchmark.py
+++ benchmark/run_1mo_benchmark.py
@@ -1,26 +1,28 @@
 import os
 from gcpy import benchmark
 
-
-# Directories (edit)
+# Benchmark information (*MUST EDIT*)
 maindir  = '/path/to/main/directory'
-refdir = os.path.join(maindir, 'ref_directory_name') 
-devdir = os.path.join(maindir, 'dev_directory_name')
-plotsdir =os.path.join(maindir,'plots_directory_name')
+refstr   = 'ref_version_string'
+devstr   = 'dev_version_string'
+datestr  = '20160701'
 
-# Files (edit)
-spcfile = 'SpeciesConc_diagnostics_filename'
-hcofile = 'HEMCO_diagnostics_filename'
+# Directories (edit as needed)
+refdir   = os.path.join(maindir, refstr) 
+devdir   = os.path.join(maindir, devstr)
+plotsdir = os.path.join(maindir, devstr, 'output')
+
+# Files (edit as needed)
+spcfile  = 'GEOSChem.SpeciesConc.' + datestr + '_0000z.nc4'
+hcofile  = 'HEMCO_diagnostics.'    + datestr + '0000.nc'
+aerfile  = 'GEOSChem.Aerosols.'    + datestr + '_0000z.nc4'
+jvfile   = 'GEOSChem.JValues.'     + datestr + '_0000z.nc4'
 
 # Paths to data
 refspc = os.path.join(maindir, refdir, spcfile)
 devspc = os.path.join(maindir, devdir, spcfile)
 refhco = os.path.join(maindir, refdir, hcofile)
 devhco = os.path.join(maindir, devdir, hcofile)
-
-# Plot strings (edit)
-refstr = 'ref_version_string'
-devstr = 'dev_version_string'
 
 # Concentration plots (includes lumped species and separates by category)
 benchmark.make_gcc_1mo_benchmark_conc_plots(refspc, refstr, devspc, devstr, dst=plotsdir, overwrite=True)
@@ -29,4 +31,4 @@ benchmark.make_gcc_1mo_benchmark_conc_plots(refspc, refstr, devspc, devstr, dst=
 benchmark.make_gcc_1mo_benchmark_emis_plots(refhco, refstr, devhco, devstr, dst=plotsdir, overwrite=True)
 
 # Emissions tables
-make_gcc_1mo_benchmark_emis_tables(refhco, refstr, devhco, devstr, dst=plotsdir, overwrite=True)
+benchmark.make_gcc_1mo_benchmark_emis_tables(refhco, refstr, devhco, devstr, dst=plotsdir, overwrite=True)
