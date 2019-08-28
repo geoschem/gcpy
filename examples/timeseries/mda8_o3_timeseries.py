5b7d92b213cd08bc56b3e001eb9e7b63b357f326
Melissa Sulprizio <mpayer@seas.harvard.edu>
2019-08-28 11:26:17 -0400

    Update MDA8 ozone timeseries example script
    The previous example scripts worked with external datasets and bpch input.
    The mad8_o3_timeseries.py script added here uses hourly netCDF output from
    GEOS-Chem. The resulting dataset is saved to a new netCDF file and plotted.
    
    This script also fixes an issue in the resample method. The usage of this
    method in the other scripts was for older versions and no longer worked.
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

--- examples/timeseries/mda8_o3_timeseries.py
+++ examples/timeseries/mda8_o3_timeseries.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-MDA8 Timeseries Calculations with xarray
+MDA8 Timeseries Calculations
 ========================================
 
 A common statistic used when constructing standards for air quality
@@ -16,35 +16,16 @@ plt.style.use(['seaborn-talk', 'seaborn-ticks'])
 
 import pandas as pd
 import xarray as xr
-import xbpch
-from dask.diagnostics import ProgressBar
-
+import numpy as np
 from os.path import join
 
-# First we need to read in some data. We'll read a multi-file ND49 BPCH
-# dataset using the xbpch package.
-dates = ["200601{:02d}".format(d) for d in range(1, 22)]
-ROOT = "/Users/daniel/workspace/bpch/test_data/"
-fns = [
-    join(ROOT, "ND49_{}_ref_e2006_m2010.bpch".format(date))
-    for date in dates
-]
-nd49_data = xbpch.open_mfbpchdataset(
-    fns, diaginfo_file="/Users/daniel/Desktop/sample_nd49/diaginfo.dat",
-    tracerinfo_file="/Users/daniel/Desktop/sample_nd49/tracerinfo.dat",
-    dask=True, memmap=True,
-)
-o3_data = nd49_data['IJ_AVG_S_O3']
-with ProgressBar():
-    print("Loading data into memory")
-    o3_data.load()
+# Read hourly data
+InFile = 'GEOSChem.Hourly_SfcO3.2017.nc'
+ds = xr.open_dataset(InFile)
+o3_data = ds['SpeciesConc_O3']
 
-# Second, we compute the 8-hour rolling averages for the ozone.
-avg_8hr_o3 = (
-    o3_data
-    .rolling(time=8, min_periods=6)
-    .mean()
-)
+# Compute the 8-hour rolling averages for ozone
+avg_8hr_o3 = (o3_data.rolling(time=8, min_periods=6).mean())
 
 # By default, this takes the last timestamp in a rolling interval; i.e. the
 # timestamps correspond to the preceding 8 hours. We want them to refer to
@@ -55,7 +36,15 @@ avg_8hr_o3.time.values[:] = times_pd
 
 # Finally, aggregate by calendar day and compute the maxima of the set of
 # 8-hour averages for each day
-mda8_o3 = avg_8hr_o3.resample('D', dim='time', how='max')
+mda8_o3 = avg_8hr_o3.resample(time='D').max(dim='time')
+mda8_o3.name='mda8_o3'
+
+# Save output to new netCDF file
+mda8_o3.to_netcdf('GEOSChem.MDA8_O3.20170.nc', 'w', format='NETCDF4',
+                  encoding={'lat': {'_FillValue': None}, 
+                            'lon': {'_FillValue': None}, 
+                            'time': {'_FillValue': None},
+                            'mda8_o3': {'_FillValue': None}})
 
 # Select data for one specific location, near Boston
 boston_mda8_o3 = mda8_o3.sel(lon=-71., lat=42., method='nearest')
