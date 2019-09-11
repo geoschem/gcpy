d4349050188c62e37626f5e13aa234dc04ee9cf2
Bob Yantosca <yantosca@seas.harvard.edu>
2019-09-11 17:46:04 -0400

    Add a function to print out the versions of dependent packages
    Added the show_versions() function in gcpy/_version.py.  This can be
    called to print out the versions of dependent packages, or to return
    the version number information as a JSON object.
    
    This commit now resolves GCPY issue #36:
    https://github.com/geoschem/gcpy/issues/36
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- gcpy/__init__.py
+++ gcpy/__init__.py
@@ -1,3 +1,6 @@
+'''
+GCPY initialization script.  Imports nested packages for convenience.
+'''
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -9,9 +12,11 @@ from .benchmark import *
 from .units import *
 from . import plot
 from . import grid
+from ._version import *
 
+# Figure this out later
 #try:
-#    from . version import __version__
+#    from .version import __version__
 #except ImportError:
 #    raise ImportError('gcpy was not properly installed; some functionality '
 #                      'may be not work. If installing from source code, '
