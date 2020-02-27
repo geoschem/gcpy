e6a998094454bd47b0e9929a886154f7be91142d
Bob Yantosca <yantosca@seas.harvard.edu>
2020-02-27 12:03:45 -0500

    Finalize updates for aerosol and TransportTracers budgets
    gcpy/benchmark.py:
    (a) Now write the OH Metrics table to the Plots/Tables folder.
    
    gcpy/budget_aer.py:
    (a) Now get the species MW's from the species database
    (b) Attempt to read the species database from the data directory (i.e.
        OutputDir); if not found, read species database from gcpy folder.
    (c) Read list of AOD diagnostics from the gcpy/aod_species.yml file
    (d) Now get MW of air from constants.py
    
    gcpy/budget_tt.py:
    (a) Now get the species MW's from the species database.
    (b) Attempt to read the species database from the data directory (i.e.
        OutputDir); if not found read species database from gcpy folder.
    (c) Now get MW of air from constants.py
    
    gcpy/constants.py:
    (a) Now redefine MW_AIR from 28.97e-3 to 28.9644e-3
    (b) Updated comments.
    
    gcpy/species_database.yml
    (a) Added entries for Pb210, Be7, Be7Strat, Be10, Be10Strat.

--- gcpy/constants.py
+++ gcpy/constants.py
@@ -1,25 +1,28 @@
-""" Physical/chemical constants """
+""" 
+Contains definitions of physical and chemical constants,
+as well as other needed global variables.
+"""
 
-#: Acceleration due to gravity, m/s2
+#: Acceleration due to gravity [m s-2]
 G = 9.80665
 
-#: Radius of the Earth, km
-# "Equal area" radius. Gives correct total surface area when modeled as a sphere
+#: "Equal area" radius of the Earth [km]
+# "Gives the correct total surface area when modeled as a sphere
 R_EARTH = 6371.0072
 
-#: Avogadro's number, molec/mol
+#: Avogadro's number [mol-1]
 AVOGADRO = 6.022140857e+23
 
-#: Universal gas contant, J/mol/K
+#: Universal gas contant [J mol-1 K-1]
 R_GAS_UNIV = 8.314
 
-#: Boltzmann's constant, J/K
+#: Boltzmann's constant [J K-1]
 K_BOLTZMANN = 1.381e-23
 
-# Typical molar mass of air, kg/mol
-MW_AIR = 28.97e-3
+# Typical molar mass of air [kg mol-1]
+MW_AIR = 28.9644e-3
 
-# Molar mass of water, kg/mol
+# Molar mass of water [kg mol-1]
 MW_H2O = 18.016e-3
 
 # netCDF variables that we should skip reading
