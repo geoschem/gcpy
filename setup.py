ecf6f7e85f5e9ed9457ef0424ce9be3d5fce88a1
Lizzie Lundgren <elundgren@seas.harvard.edu>
2022-09-22 13:08:00 -0400

    Add sparselt package as a gcpy requirement
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- setup.py
+++ setup.py
@@ -92,8 +92,9 @@ setup(
     packages = find_packages(),
     include_package_data=True,
     install_requires=["xesmf>=0.2.1", "scipy>=1.3.1", "Cartopy>=0.17.0", "pandas>=0.25.1",
-                      "matplotlib>=3.1.1", "tabulate>=0.8.3", "joblib>=0.17.0", "xbpch>=0.3.5", 
-                      "numpy>=1.19.1", "PyPDF2>=1.26.0", "sphinx", "sphinx_rtd_theme", "sphinx-autoapi"],
+                      "matplotlib>=3.1.1", "tabulate>=0.8.3", "joblib>=0.17.0", "xbpch>=0.3.5",
+                      "numpy>=1.19.1", "PyPDF2>=1.26.0", "sphinx", "sphinx_rtd_theme",
+                      "sphinx-autoapi", "sparselt>=0.1.3"],
     classifiers = CLASSIFIERS
 )
 
