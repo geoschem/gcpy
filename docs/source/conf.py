906163f5a2774ee7e91d2ed9446f312c8568bc55
Bob Yantosca <yantosca@seas.harvard.edu>
2022-10-17 09:40:59 -0400

    Minor doc updates (regridding, release guide); updated version in conf.py
    docs/source/conf.py
    - Set version number to 1.3.0
    
    docs/source/Regridding
    - Update sparselt example to be consistent with text above it
    - Trimmed trailing whitespace
    
    docs/source/Release_Guide
    - Fixed incorrect indentation of |br|
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, GEOS-Chem Support Team'
 author = 'GEOS-Chem Support Team'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.0-rc.0'
+release = '1.3.0'
 
 # -- General configuration ---------------------------------------------------
 
