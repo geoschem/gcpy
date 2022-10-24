e9c369516f257bb3a9bb57f547cf593208ecadc6
Bob Yantosca <yantosca@seas.harvard.edu>
2022-10-24 14:26:28 -0400

    Add example for regridding 4x5 restart file to cubed-sphere resolution
    examples/working_with_files/regrid_restart_ll_to_cs.py
    - Added example script for regridding GEOS-Chem Classic restart files
      from 4x5 degree resolution (aka latlon46x72) to the various cubed-sphere
      resolutions using sparselt.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

---
+++ examples/working_with_files/regrid_restart_ll_to_cs.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""
+Regrids a 4x5 GEOS-Chem Classic restart file to cubed-sphere resolutions.
+"""
+
+# Imports
+from os.path import join
+import xarray as xr
+import sparselt.esmf
+import sparselt.xr
+
+# Path to regridding weights (EDIT AS NEEDED)
+weights_dir="/path/to/regridding/weights/"
+
+# List of simulation types (EDIT AS NEEDED)
+simulation_list = ["carboncycle"]
+
+# List of months (EDIT AS NEEDED)
+month_list = ["01", "07"]
+
+# List of cubed-sphere grids (EDIT AS NEEDED)
+cubed_sphere_grid_list = ["c24", "c48", "c90", "c180", "c360"]
+
+# Loop over simulation types
+for sim in simulation_list:
+
+    # Loop over months
+    for mm in month_list:
+
+        # Read input data
+        infile = f"GEOSChem.Restart.{sim}.2019{mm}01_0000z.nc4"
+        print(f"Reading {infile}")
+        ds_in = xr.open_dataset(infile)
+
+        # Loop over cubed-sphere grids
+        for cs in cubed_sphere_grid_list:
+
+            # Number of grid points per side
+            cs_res = int(cs[1:])
+    
+            # Regridding transform file
+            regrid_file = f"regrid_weights_latlon46x72_to_{cs}.nc"
+            weights_file = join(weights_dir, regrid_file)
+   
+            # Create a linear transform object from the regridding
+            # weights file for the combination of source and target
+            # horizontal resolutions.  NOTE: GCHP restart files use
+            # a grid where lat = 6*cs_res.
+            transform = sparselt.esmf.load_weights(
+                weights_file,
+                input_dims=[('lat', 'lon'), (46, 72)],
+                output_dims=[('lat', 'lon'), (6*cs_res, cs_res)]
+            )
+            
+            # Regrid to cubed-sphere
+            ds_out = sparselt.xr.apply(transform, ds_in)
+            
+            # Write to output resolution 
+            outfile = f"GEOSChem.Restart.{sim}.2019{mm}01_0000z.{cs}.nc4"
+            print(f"Writing {outfile}")
+            ds_out.to_netcdf(outfile)
+
+            # Cleanup
+            del transform
+            del ds_out
+
+        # Cleanup
+        del ds_in
