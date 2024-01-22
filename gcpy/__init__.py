39100b02eafb64e941c5058dc2e76d0f79545d60
Hannah O Nesser <hannah.nesser@gmail.com>
2024-01-22 09:40:50 -0800

    Removing too large demo file

--- gcpy/__init__.py
+++ gcpy/__init__.py
@@ -13,18 +13,18 @@ except ImportError:
                       '\nElse, please reinstall using your package manager.')
 
 from .util import *
-from .date_time import *
-from .units import *
-from .ste_flux import *
-from .regrid import *
-from .plot import *
-from .oh_metrics import *
-from .mean_oh_from_logs import *
-from .grid import *
+# from .date_time import *
+# from .units import *
+# from .ste_flux import *
+# from .regrid import *
+# from .plot import *
+# from .oh_metrics import *
+# from .mean_oh_from_logs import *
+# from .grid import *
 from .constants import *
-from .budget_tt import *
-from .benchmark import *
-from .file_regrid import *
-from .grid_stretching_transforms import *
-from .cstools import *
+# from .budget_tt import *
+# from .benchmark import *
+# from .file_regrid import *
+# from .grid_stretching_transforms import *
+# from .cstools import *
 from .format_hemco_data import *
