b258b3940cd32f9b5b5673b4d32ce54ab972c5ff
Bob Yantosca <yantosca@seas.harvard.edu>
2024-05-03 13:31:43 -0400

    Add benchmark script to scrape GEOS-Chem Classic timing information
    gcpy/benchmark/modules/benchmark_scrape_gcclassic_timers.py
    - Benchmark module that can scrape the timers information from either
      gcclassic_timers.json or GEOS-Chem Classic log files.  If multiple
      files are supplied as input, the timers information will be summed
      together.
    
    CHANGELOG.md
    - Updated accordingly
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- gcpy/benchmark/modules/benchmark_scrape_gcclassic_timers.py
+++ gcpy/benchmark/modules/benchmark_scrape_gcclassic_timers.py
@@ -1,74 +1,114 @@
 #!/usr/bin/env python3
 """
+Scrapes GEOS-Chem Classic benchmark timing information from one or
+more JSON or text files.
 """
 import os
-from gcpy.util import verify_variable_type
 import json
+from gcpy.util import make_directory, replace_whitespace, verify_variable_type
 
 
-def read_gcclassic(ifile):
+def read_gcclassic(input_files):
     """
-    Determines if the input is a valid JSON.
+    Determines whether we should call a function to parse the given
+    input file(s) as JSON or plain text.
 
     Args
-    ifile  : str  : file name
+    input_files : str|list     : File or list of files to parse
 
     Returns
-    result : dict : Dictionary with timing information
+    result      : list of dict : List of dicts with timing info
     """
-
-    # Make sure file exists
-    if not os.path.exists(ifile):
-        raise FileNotFoundError(f"Could not find {ifile}!")
-
-    # First try to read the file as a JSON,
-    # then try to read the file as text.
     try:
-        result = read_gcclassic_json(ifile)
-    except ValueError as err:
-        result = read_gcclassic_log(ifile)
+        result = read_timing_data(input_files, read_one_json_file)
+    except ValueError:
+        result = read_timing_data(input_files, read_one_text_file)
     return result
 
 
-def read_gcclassic_json(
-        ifile
+def read_timing_data(
+        input_files,
+        reader,
 ):
     """
     Parses the GEOS-Chem Classic timing information in JSON format
     and returns a dictionary with the results.
 
     Args
-    ifile  : str  : File name
+    input files : str|list     : JSON or text file(s) to parse
 
     Returns
-    result : dict : Dictionary with timing information
+    timing      : list of dict : Dictionary with timing information
     """
+    # Return value
+    timing = []
+
+    # If more than one file has been provided, read the timing
+    # information and return a list of dictionaries with results
+    if isinstance(input_files, list):
+        for input_file in input_files:
+            result = reader(input_file)
+            timing.append(result)
+        return timing
+
+    # If only one file has been provided, then read it
+    # and return the dictionary in a list
+    if isinstance(input_files, str):
+        result = reader(input_files)
+        timing.append(result)
+        return timing
+
+    raise ValueError("Argument 'input_files' is not of type str or list!")
+
+
+def read_one_json_file(json_file):
+    """
+    Parses a GEOS-Chem JSON file with timing information
+    and returns a dictionary with the results.
+
+    Args
+    json_file : str  : JSON file with timing information
+
+    Returns
+    result    : dict : Dictionary with timing information
+    """
+
+    # Make sure file exists
+    if not os.path.exists(json_file):
+        raise FileNotFoundError(f"Could not find {json_file}!")
+
+    # If the file is not a JSON file, raise a ValueError, as
+    # this will prompt read_gcclassic to parse the file as text.
     try:
-        with open(ifile, encoding="utf-8") as json_file:
-            result = json.load(json_file)
+        with open(json_file, encoding="utf-8") as ifile:
+            result = json.load(ifile)
             return result["GEOS-Chem Classic timers"]
     except ValueError as err:
         raise ValueError from err
 
 
-def read_gcclassic_log(ifile):
+def read_one_text_file(text_file):
     """
-    Parses the GEOS-Chem Classic log file with timing information
-    and returns a dictionary with the results.
+    Parses the GEOS-Chem Classic log file (plain text) with
+    timing information and returns a dictionary with the results.
 
     Args
-    ifile  : str  : File name
+    text_file : str  : Text file with timing information
 
     Returns
-    result : dict : Dictionary with timing information
+    result    : dict : Dictionary with timing information
     """
     keep_line = False
     timers = {}
 
-    # Read the line backwards and get just keep the timing information
-    with open(ifile, encoding="utf-8") as log_file:
+   # Make sure file exists
+    if not os.path.exists(text_file):
+        raise FileNotFoundError(f"Could not find {text_file}!")
 
-        for line in reversed(list(log_file)):
+    # Read the line backwards and get just keep the timing information
+    with open(text_file, encoding="utf-8") as ifile:
+
+        for line in reversed(list(ifile)):
             line = line.strip("\n")
 
             # Set a flag to denote the start & end of timing info
@@ -88,11 +128,48 @@ def read_gcclassic_log(ifile):
     return timers
 
 
+def sum_timers(timers):
+    """
+    Sums the time in seconds for each GEOS-Chem timer.  Input may be
+    a single dict with timing information or a list of dicts.
+
+    Args
+    timers : dict|list : GEOS-Chem timing information from one or more
+                         JSON or log files.
+
+    Returns
+    result : dict      : Sum of timing information
+    """
+
+    # If timers is of type dict, no summing is needed.
+    if isinstance(timers, dict):
+        return timers
+
+    # If timers is a list of dicts, sum the times
+    # in seconds into a new dict, and then return.
+    if isinstance(timers, list):
+
+        # Initialize the result dict
+        result = {}
+        for timer in timers:
+            for (key, val) in timer.items():
+                result[key] = 0.0
+
+        # Then sum the time in seconds for each timer
+        for timer in timers:
+            for (key, val) in timer.items():
+                result[key] += float(val["seconds"])
+
+        return result
+
+    raise ValueError("Argument 'timers' must be of type str or dict!")
+
+
 def print_timer(key, ref, dev, ofile):
     """
     Prints timing info for a single timer to a log file.
     """
-    line = f"{key:<25}  {ref[key]['seconds']:>20}  {dev[key]['seconds']:>20}"
+    line = f"{key:<25}  {ref[key]:>20.3f}  {dev[key]:>20.3f}"
     print(line, file=ofile)
 
 
@@ -107,61 +184,95 @@ def display_timers(ref, ref_label, dev, dev_label, table_file):
     with open(table_file, "w", encoding="utf-8") as ofile:
 
         # Print header
-        print(f"{'Timer':<25}  {ref_label:>20}  {dev_label:>20}", file=ofile)
-        print(f"{'-'*25:<25}  {'-'*20:>20}  {'-'*20:>20}", file=ofile)
-        
+        print("%"*79, file=ofile)
+        print("%%% GEOS-Chem Classic Benchmark Timing Information",
+              file=ofile)
+        print("%%%", file=ofile)
+        print(f"%%% Ref = {ref_label}", file=ofile)
+        print(f"%%% Dev = {dev_label}", file=ofile)
+        print("%"*79, file=ofile)
+        print("\n", file=ofile)
+        print(f"{'Timer':<25}  {'Ref [s]':>20}  {'Dev [s]':>20}", file=ofile)
+        print("-"*79, file=ofile)
+
         # Print timers
-        print_timer("GEOS-Chem",             ref, dev, ofile) 
-        print_timer("HEMCO",                 ref, dev, ofile)     
-        print_timer("All chemistry",         ref, dev, ofile) 
-        print_timer("=> Gas-phase chem",     ref, dev, ofile)  
-        print_timer("=> Photolysis",         ref, dev, ofile) 
-        print_timer("=> Aerosol chem",       ref, dev, ofile) 
-        print_timer("=> Linearized chem",    ref, dev, ofile)  
-        print_timer("Transport",             ref, dev, ofile) 
+        print_timer("GEOS-Chem",             ref, dev, ofile)
+        print_timer("HEMCO",                 ref, dev, ofile)
+        print_timer("All chemistry",         ref, dev, ofile)
+        print_timer("=> Gas-phase chem",     ref, dev, ofile)
+        print_timer("=> Photolysis",         ref, dev, ofile)
+        print_timer("=> Aerosol chem",       ref, dev, ofile)
+        print_timer("=> Linearized chem",    ref, dev, ofile)
+        print_timer("Transport",             ref, dev, ofile)
         print_timer("Convection",            ref, dev, ofile)
-        print_timer("Boundary layer mixing", ref, dev, ofile) 
+        print_timer("Boundary layer mixing", ref, dev, ofile)
         print_timer("Dry deposition",        ref, dev, ofile)
         print_timer("Wet deposition",        ref, dev, ofile)
         print_timer("Diagnostics",           ref, dev, ofile)
         print_timer("Unit conversions",      ref, dev, ofile)
 
-        
+
 def make_benchmark_timing_table(
-        ref_file,
+        ref_files,
         ref_label,
-        dev_file,
+        dev_files,
         dev_label,
-        dst,
+        dst="./benchmark",
+        overwrite=False,
 ):
     """
+    Creates a table of timing information for GEOS-Chem Classic
+    benchmark simulations given one or more JSON and/or text files
+    as input.
+
+    Args:
+
     """
-    verify_variable_type(ref_file, (str, list))
+    verify_variable_type(ref_files, (str, list))
     verify_variable_type(ref_label, str)
-    verify_variable_type(dev_file, (str, list))
+    verify_variable_type(dev_files, (str, list))
     verify_variable_type(dev_label, str)
     verify_variable_type(dst, str)
 
-    # Strip timing info from JSON or log ifle
-    ref_timers = read_gcclassic(ref_file)
-    dev_timers = read_gcclassic(dev_file)
+    # Create the destination folder
+    make_directory(dst, overwrite)
+
+    # Strip timing info from JSON/text file(s) and sum the them.
+    ref_timers = sum_timers(read_gcclassic(ref_files))
+    dev_timers = sum_timers(read_gcclassic(dev_files))
+
+    # Filename for output
+    timing_table = replace_whitespace(
+        os.path.join(
+            dst,
+            f"Benchmark_Timers_{ref_label}_vs_{dev_label}.txt"
+        )
+    )
 
     # Write timing info to a table
     display_timers(
         ref_timers,
-        ref_label,
+        replace_whitespace(ref_label),
         dev_timers,
-        dev_label,
-        "sample_output.txt",
+        replace_whitespace(dev_label),
+        timing_table,
     )
 
 
 if __name__ == '__main__':
+
+    REF_FILES = [
+        "./gcclassic_timers.json",
+        "./gcclassic_timers.json"
+    ]
+    DEV_FILES = "./execute.gc_4x5_merra2_fullchem_benchmark.log"
+
+    # Debug test
     make_benchmark_timing_table(
-       "./gcclassic_timers.json",
-        "GCC 14.4.0",
-        "./execute.gc_4x5_merra2_fullchem_benchmark.log",
-        "GCHP 14.4.0",
-        "./"
-    )
-#        "./execute.gchp_merra2_fullchem_benchmark.log",
+        REF_FILES,
+        "GCC 14.4.0 json",
+        DEV_FILES,
+        "GCC 14.4.0 log",
+        dst="./",
+        overwrite=True,
+)
