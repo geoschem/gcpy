Fix computation of dynamic ratio colorbar min/max values
gcpy/plot/six_plot.py
- Now set vmin as the min of the absolute values of the min and max
  of the plot_val array and set vmax as 1/vmin.
- This should prevent the colorbar from being set to a too-narrow range.

CHANGELOG.md
- Updated accordingly.

gcpy/plot/six_plot.py
@@ -544,10 +544,10 @@ def vmin_vmax_for_ratio_plots(
     """
     # Ratio (dynamic range) subplot)
     if subplot in "dyn_ratio":
-        vmax = np.max(
+        vmin = np.min(
             [np.abs(np.nanmin(plot_val)), np.abs(np.nanmax(plot_val))]
         )
-        vmin = 1.0 / vmax
+        vmax = 1.0 / vmin
         if vmin > vmax:
             vmin, vmax = vmax, vmin
         verbose_print(verbose, rowcol, vmin, vmax)