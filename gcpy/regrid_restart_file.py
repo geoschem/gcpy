f2fa2c4770f55cd11b6d5e43ed4cb58edfe2011d
Lizzie Lundgren <elundgren@seas.harvard.edu>
2025-10-02 10:26:04 -0400

    Do not allow resizing output template in regrid_restart_file
    This is a temporary fix to prevent users from passing an output template
    file that differs in dimension sizes from the target grid size when
    doing offline regridding. Code exists to reside the output template
    but it currently does not work.
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- gcpy/regrid_restart_file.py
+++ gcpy/regrid_restart_file.py
@@ -377,6 +377,13 @@ def regrid(dataset, output_template, weights_file):
         np.prod(output_template_shape) != weights.dst_grid_dims.item()
     )
     if resize_output_template:
+        error_message = (
+            "GCHP output template grid resolution must be the same size "
+            "as the target grid! For example, if creating restart file "
+            " with parameters c24 and stretch factor 10, the template "
+            " GCHP file must be c24 with any or no stretch factor."
+        )
+        raise ValueError(error_message)
         if is_gchp_restart_file(output_template):
             # This is useful for stretched-grid simulations because they usually
             # don't have a "normal" grid size
