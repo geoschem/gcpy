geoschem/gcpy

commit 2b0c6ada4cd57f8676fd9f875f4ba7e7a4f99e38

Add modifications suggested by Pylint to gcpy/cstools.py
gcpy/cstools.py
- Updated code to import and use the renamed "csgrid_gmao" function
  from "gcpy/grid.py"
- Modified the assigment statement for x_lat to use "i_vert in (0, 3)"
- Fixed incorrect indentation at line 753

CHANGELOG.md
- Updated accordingly

Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

gcpy/cstools.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Contains tools for working with cubed-sphere data.
 
@@ -173,7 +174,7 @@ def face_area(
             xyz_corner = np.zeros((4,3))
             for i_vert in range(4):
                 x_lon = i_lon + (i_vert > 1)
-                x_lat = i_lat + (i_vert == 0 or i_vert == 3)
+                x_lat = i_lat + i_vert in (0, 3)
                 lon_corner[i_vert] = lon_b_rad[x_lon,x_lat]
                 lat_corner[i_vert] = lat_b_rad[x_lon,x_lat]
             for i_vert in range(4):
@@ -354,7 +355,7 @@ def gen_grid(
             target_lat
         )
     else:
-        cs_temp = gcpy.csgrid_GMAO(n_cs)
+        cs_temp = gcpy.csgrid_gmao(n_cs)
 
     return xr.Dataset(
         {'nf':     (['nf'], np.array(range(6))),
@@ -750,7 +751,7 @@ def is_cubed_sphere_rst_grid(data):
     # NOTE: in DataArray objects, dims is a tuple but not a dict!
     # Comparing the len of the lat & lon coords will work for both.
     if "lat" in data.coords:
-         return len(data.coords["lat"]) == len(data.coords["lon"]) * 6
+        return len(data.coords["lat"]) == len(data.coords["lon"]) * 6
 
     # Dataset: search data.data_vars for "SPC_"
     # DataArray: search data.name for "SPC_"

commit afbe257eab49aba6ff57e15c5271665b17291472

Bug fix: add parentheses in face_area to force operator order
gcpy/cstools.py
- Changed x_lat = i_lat + i_vert in (0, 3)
  to      x_lat = i_lat + (i_vert in (0, 3))
  in order to enforce proper evaluation order.

This fixes an error that was introduced in commit 2b0c6ad, when we
added some improvements suggested by Pylint.

Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

gcpy/cstools.py
@@ -174,7 +174,7 @@ def face_area(
             xyz_corner = np.zeros((4,3))
             for i_vert in range(4):
                 x_lon = i_lon + (i_vert > 1)
-                x_lat = i_lat + i_vert in (0, 3)
+                x_lat = i_lat + (i_vert in (0, 3))
                 lon_corner[i_vert] = lon_b_rad[x_lon,x_lat]
                 lat_corner[i_vert] = lat_b_rad[x_lon,x_lat]
             for i_vert in range(4):