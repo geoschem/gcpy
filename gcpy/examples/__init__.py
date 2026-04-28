8db26ac86b3f05f3e5d667b8ba7631c8f93bee97
Bob Yantosca <yantosca@seas.harvard.edu>
2026-04-28 14:23:21 -0400

    Update additional __init__.py files
    gcpy/examples/__init__.py
    - Remove imports statements, these will be handled in sub-folders
    
    gcpy/examples/hemco/__init__.py
    - Added import statements
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- gcpy/examples/__init__.py
+++ gcpy/examples/__init__.py
@@ -1,11 +1,3 @@
 """
 User examples that demonstrate the capabilities of GCPy.
 """
-from .diagnostics import *
-from .dry_run import *
-from .grids import *
-from .hemco import *
-from .plotting import *
-from .timeseries import *
-from .working_with_files import *
-from .xarray_examples import *
--- gcpy/examples/hemco/__init__.py
+++ gcpy/examples/hemco/__init__.py
@@ -1,3 +1,6 @@
 """
 GCPy examples related to HEMCO (The Harmonized Emissions Component).
 """
+from .format_hemco_demo import *
+from .make_hemco_sa_spec import *
+from .make_mask_file import *
